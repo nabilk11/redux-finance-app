a15b4ea030fce528cd77233244eda2935f684fc7
nabilk11 <khan.nabiln11.com>
2023-08-26 02:15:36 -0400

    created navbar component within screens folder with material ui fingerprint icon - app renamed to Fingerprint Finance

---
+++ client/src/screens/navbar/index.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Box, Typography, useTheme } from "@mui/material";
+import FingerprintIcon from "@mui/icons-material/Fingerprint";
+import FlexBetween from "@/components/FlexBetween";
+
+// type Props = {};
+
+const Navbar = () => {
+  const { palette } = useTheme();
+
+  const [selected, setSelected] = useState("dashboard");
+  return (
+    <FlexBetween mb={"0.25rem"} p={"0.5rem 0rem"} color={palette.grey[300]}>
+      <FlexBetween gap={"0.68rem"}>
+        <FingerprintIcon sx={{ fontSize: "large" }} />
+        <Typography variant="h4" fontSize={"large"}>
+          Fingerprint Finance
+        </Typography>
+      </FlexBetween>
+      <FlexBetween gap={"2rem"}>
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link
+            to={"/"}
+            onClick={() => setSelected("dashboard")}
+            style={{
+              color: selected === "dashboard" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            {" "}
+            dashboard
+          </Link>
+        </Box>
+
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link
+            to={"/"}
+            onClick={() => setSelected("prediction")}
+            style={{
+              color: selected === "prediction" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            {" "}
+            prediction
+          </Link>
+        </Box>
+      </FlexBetween>
+    </FlexBetween>
+  );
+};
+
+export default Navbar;
