b7b1f91b202fcc5c0c458723c6d297238b50ab57
nabilk11 <khan.nabiln11.com>
2023-08-30 23:50:26 -0400

    added types and edited redux payload data

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,18 +1,17 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
+import { GetKpisResponse } from "./types";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
-    reducerPath: "main",
-    tagTypes: [ "Kpis"],
-    endpoints: (build)=> ({
-        getKpis:build.query<void, void>({
-            query: ()=> "kpi/kpis/",
-            providesTags: ["Kpis"],
-        }),
-    })
-
-})
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  reducerPath: "main",
+  tagTypes: ["Kpis"],
+  endpoints: (build) => ({
+    getKpis: build.query<Array<GetKpisResponse>, void>({
+      query: () => "kpi/kpis/",
+      providesTags: ["Kpis"],
+    }),
+  }),
+});
 
 // kpis query hook
-export const { useGetKpisQuery} = api;
+export const { useGetKpisQuery } = api;
