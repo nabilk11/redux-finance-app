25980ff4c20a91f79dd4a778f05e2556fdcf2e3e
nabilk11 <khan.nabiln11.com>
2023-08-30 22:11:12 -0400

    installed serverside dependencies, created express server index.js, configured mongoDB, added .env, and created data folder with dummyData.js for testing

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+
+/* CONFIGURATION */
+dotenv.config();
+
+const app = express();
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(morgan("common"));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* MONGOOSE DB CONNECTION */
+const PORT = process.env.PORT || 8080;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => {
+      console.log(`MongoDB is Now Connected & Server is Live on Port: ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(`Error connecting to Server: ${err}`));
